a9b6dc28d0a94c04c27e193273b4d9e782467991
talitamarques85 <talitadecor@gmail.com>
2025-10-20 21:18:58 -0400

    adiciona a primeira versao do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const router = express.Router()
+const app = express()
+
+const porta = 3333
+
+const mulheres = [
+
+    {
+        nome: 'Simara Conceição',
+        imagem: 'https://github.com/simaraconceicao.png',
+        minibio: 'Desenvolvedora e Instrutora'
+    },
+    {   
+        nome: 'Iana Chan',
+        imagem: 'https://bit.ly/3LJIyOF',
+        minibio: 'Fundadora Programaria'
+    },
+    {   
+        nome: 'Nina da Hora',
+        imagem: 'https://bit.ly/3FKpFaz',
+        minibio: 'Hacker antirracista'
+    }
+]
+
+function mostraMulheres (requeste, response){
+
+    response.json(mulheres)
+}
+
+
+function mostraPorta () {
+    console.log("servidor criado e rodando na porta", porta)
+}
+
+
+app.use(router.get('/mulheres', mostraMulheres))
+app.listen(porta, mostraPorta)
